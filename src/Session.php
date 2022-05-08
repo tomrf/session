611b5f1195a68bd172aef5c2a271b2d69d5a1100
Tom Rune Flo <tom@upnode.no>
2022-05-08 18:54:41 +0200

    initial commit

---
+++ src/Session.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomrf\Session;
+
+class Session
+{
+    private bool $isClosedForWriting = false;
+
+    /**
+     * Set a session key.
+     */
+    public function set(string $key, mixed $value): void
+    {
+        $this->startSessionIfNotStarted();
+        $this->throwExceptionIfClosedForWriting();
+
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Get a session key value.
+     *
+     * @param null|mixed $default
+     */
+    public function get(string $key, $default = null): mixed
+    {
+        $this->startSessionIfNotStarted();
+        $this->throwExceptionIfClosedForWriting();
+
+        return $_SESSION[$key] ?? $default;
+    }
+
+    /**
+     * Delete a session key.
+     */
+    public function delete(string $key): void
+    {
+        $this->startSessionIfNotStarted();
+        $this->throwExceptionIfClosedForWriting();
+
+        if (isset($_SESSION[$key])) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    /**
+     * Clear the session.
+     */
+    public function clear(): void
+    {
+        $this->startSessionIfNotStarted();
+        $this->throwExceptionIfClosedForWriting();
+
+        foreach (array_keys($_SESSION) as $key) {
+            unset($_SESSION[$key]);
+        }
+    }
+
+    /**
+     * Close session for writing.
+     */
+    public function closeWrite(): void
+    {
+        $this->startSessionIfNotStarted();
+
+        if (true !== session_write_close()) {
+            throw new SessionException('Failed to close session for writing');
+        }
+
+        $this->isClosedForWriting = true;
+    }
+
+    /**
+     * Start session.
+     */
+    public function startSession(): void
+    {
+        $this->startSessionIfNotStarted();
+    }
+
+    private function startSessionIfNotStarted(): void
+    {
+        if (session_id()) {
+            return;
+        }
+
+        session_start();
+    }
+
+    private function throwExceptionIfClosedForWriting(): void
+    {
+        if ($this->isClosedForWriting) {
+            throw new SessionException('Unable to write to session, session is closed for writing');
+        }
+    }
+}
---
+++ tests/SessionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+use Tomrf\Session\Session;
+use Tomrf\Session\SessionException;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class SessionTest extends \PHPUnit\Framework\TestCase
+{
+    public static Session $session;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$session = new Session();
+        self::$session->startSession();
+    }
+
+    public function testSessionIsInstanceOfSession(): void
+    {
+        static::assertInstanceOf(Session::class, self::$session);
+    }
+
+    public function testSessionSetAndGet(): void
+    {
+        $data = [
+            'string' => 'a string',
+            'int' => 123,
+            'float' => 1234.12345,
+            'bool' => true,
+            'array' => ['array' => 1],
+            'object' => new stdClass(),
+            'null' => null,
+        ];
+
+        foreach ($data as $key => $value) {
+            $this->session()->set($key, $value);
+            static::assertSame($value, $this->session()->get($key));
+        }
+    }
+
+    public function testSessionClear(): void
+    {
+        $this->session()->set('k1', 1);
+        $this->session()->set('k2', 'a');
+        $this->session()->set('k3', false);
+
+        $this->session()->clear();
+
+        static::assertNull($this->session()->get('k1'));
+        static::assertNull($this->session()->get('k2'));
+        static::assertNull($this->session()->get('k3'));
+    }
+
+    public function testSessionCloseWrite(): void
+    {
+        $this->expectException(SessionException::class);
+        $this->session()->closeWrite();
+        $this->session()->set('testCloseWrite', true);
+    }
+
+    private function session(): Session
+    {
+        return self::$session;
+    }
+}
